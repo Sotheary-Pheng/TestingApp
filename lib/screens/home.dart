2f06b582f7abeec593926780710381d3aa21b77b
Sotheary-Pheng <phengsotheary9@gmail.com>
2025-05-10 14:21:27 +0700

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:app_testing/models/album.dart';
+import 'package:app_testing/repo/crud.dart';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late Future<Album> futureAlbum;
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<Album>(
+        future: futureAlbum,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data!.title);
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
